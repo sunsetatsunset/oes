6d308086f976ffe3a422be0b23a2dd5452af5ce2
sunsetatsunset <321099983@qq.com>
2021-07-22 16:59:57 +0800

    hot_fix commit 2 conflict

--- src/main/java/com/minghao/oes/GitTest.java
+++ src/main/java/com/minghao/oes/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("a test");
         System.out.println("a test 222");
+        System.out.println("a test conflict");
     }
 }
