08aee303760ee97c77e6956bece28e34acb55046
sunsetatsunset <321099983@qq.com>
2021-07-22 16:57:41 +0800

    hot_fix commit 1

--- src/main/java/com/minghao/oes/GitTest.java
+++ src/main/java/com/minghao/oes/GitTest.java
@@ -3,5 +3,6 @@ package com.minghao.oes;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("a test");
+        System.out.println("a test 222");
     }
 }
